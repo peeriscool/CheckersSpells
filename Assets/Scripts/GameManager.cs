0f252eda91e578a8272cffc936135c4144843e58
peer <masterfruitieapple@hotmail.com>
2021-09-27 21:34:35 +0200

    Fixed merge conflict, plz add checkers!

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,17 +5,51 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     Checker checkerTest;
+    public Card_ScriptableObject[] Cards;
+    InventoryManager Hand;
     // Start is called before the first frame update
     void Start()
     {
         GridSystem.SetGridSize(8, 8);
         checkerTest = new Checker(new GridPos(5, 7));
         Debug.Log(GridSystem.checkGridPosition(new GridPos(5, 7)));
+        Hand = new InventoryManager(Cards, 10);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Hand.StartTurn();
+        }
     }
 }
+
+/*
+ *using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GridSystem grid;
+    InventoryManager Hand;
+    public Card_ScriptableObject[] Cards;
+
+    public delegate Card_ScriptableObject DrawACard();
+    void Start()
+    {
+        Hand = new InventoryManager(Cards, 10);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Hand.StartTurn();            
+        }
+    }
+}
+ 
+ */
